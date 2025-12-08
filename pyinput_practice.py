54a8a3df6dcfb46af66d6cf914ad3257a16bf200
tiriboy <esromtadesse7@gmail.com>
2025-12-07 19:09:43 -0800

    A function that gives 5 attempts to enter an integer whose digits sum is 10

--- pyinput_practice.py
+++ pyinput_practice.py
@@ -7,9 +7,9 @@ def sum_10(string):
        digits.append(int(digit))
      if sum(digits) == 10:
        return 'Correct'
-     return 'Incorrect'
-   except:
-       raise Exception('Invalid Input')
+     raise Exception('Sum is not 10')
+   except Exception as e:
+       raise Exception(str(e))
    
 try:
   result = pyinputplus.inputCustom(sum_10,prompt='Enter a number whose digits add up to 10 (5 attempts): ', limit=5)
